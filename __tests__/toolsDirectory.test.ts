be10de90d069a84d60758eb31d00ff9e79c50120
Enrico Campidoglio <enrico.campidoglio@gmail.com>
2019-10-24 11:49:12 +0200

    Adds a simple implementation of the Cake action

---
+++ __tests__/toolsDirectory.test.ts
@@ -0,0 +1,47 @@
+import { ToolsDirectory } from "../src/toolsDirectory";
+
+describe('When constructing the tools directory', () => {
+  test('it should set the default directory path to tools', () => {
+    const sut = new ToolsDirectory();
+    expect(sut.path).toBe('tools');
+  });
+
+  test('it should set the directory path to the specified value', () => {
+    const sut = new ToolsDirectory('theName');
+    expect(sut.path).toBe('theName');
+  });
+
+  test('it should remove ./ at the beginning of the specified path', () => {
+    const sut = new ToolsDirectory('./theName');
+    expect(sut.path).toBe('theName');
+  });
+
+  test('it should keep ../ at the beginning of the specified path', () => {
+    const sut = new ToolsDirectory('../theName');
+    expect(sut.path).toBe('../theName');
+  });
+
+  test('it should remove double slashes from the specified path', () => {
+    const sut = new ToolsDirectory('the//name');
+    expect(sut.path).toBe('the/name');
+  });
+});
+
+describe('When appending a file name', () => {
+  test('it should join the directory path with the specified file name', () => {
+    const sut = new ToolsDirectory();
+    expect(sut.appendFileName('theFileName')).toBe(`${sut.path}/theFileName`);
+  });
+
+  test('it should remove any extra slashes in front of the specified file name', () => {
+    const sut = new ToolsDirectory();
+    expect(sut.appendFileName('/theFileName')).toBe(`${sut.path}/theFileName`);
+  });
+});
+
+describe('When converting the tools directory into a string', () => {
+  test('it should return the path of the directory', () => {
+    const sut = new ToolsDirectory();
+    expect(sut.toString()).toBe(sut.path);
+  });
+});
---
+++ src/toolsDirectory.ts
@@ -0,0 +1,26 @@
+import * as io from '@actions/io';
+import * as path from 'path';
+
+export class ToolsDirectory {
+  private readonly _path: string;
+
+  constructor(path: string = 'tools') {
+    this._path = path;
+  }
+
+  get path(): string {
+    return path.normalize(this._path);
+  }
+
+  create() {
+    return io.mkdirP(this.path);
+  }
+
+  appendFileName(fileName: string): string {
+    return path.join(this.path, fileName);
+  }
+
+  toString(): string {
+    return this.path;
+  }
+}
