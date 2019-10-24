c572f7604de0d7b8ddd5cbfe158dc9bff68f300c
Enrico Campidoglio <enrico.campidoglio@gmail.com>
2019-10-24 23:24:15 +0200

    Allows to specify the task to run in the script

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,18 @@
 import * as core from '@actions/core';
 import { ToolsDirectory } from "./toolsDirectory";
 import { CakeTool } from "./cake";
+import { CakeArgument } from './cakeParameter';
 
 export async function run() {
   try {
     const scriptPath = core.getInput('script-path');
+    const target = new CakeArgument('target', core.getInput('target'));
 
     const toolsDir = new ToolsDirectory();
     await toolsDir.create();
 
     await CakeTool.installLocally(toolsDir);
-    await CakeTool.runScript(scriptPath, toolsDir);
+    await CakeTool.runScript(scriptPath, toolsDir, target);
   } catch (error) {
     core.setFailed(error.message);
   }
