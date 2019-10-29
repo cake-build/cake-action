7117ea876084bce8ba5a614189c5043fd2c823f4
Mattias Karlsson <matkar@wcom.se>
2019-10-29 16:49:05 +0100

    (GH-1) Add specifying/pinning Cake version support * fixes #1

--- src/dotnet.ts
+++ src/dotnet.ts
@@ -1,9 +1,11 @@
+import * as path from 'path';
 import * as core from '@actions/core';
 import { exec } from '@actions/exec';
 import { ToolsDirectory } from './toolsDirectory';
 import { Platform } from './platform';
 
 const dotnetToolInstall = 'dotnet tool install';
+const dotnetToolUnInstall = 'dotnet tool uninstall';
 const dotnetCake = 'dotnet-cake';
 
 export class DotNet {
@@ -11,22 +13,53 @@ export class DotNet {
     core.exportVariable('DOTNET_CLI_TELEMETRY_OPTOUT', '1');
   }
 
-  static async installLocalCakeTool(targetDirectory: ToolsDirectory = new ToolsDirectory()) {
-    const cakeTool = Platform.isWindows() ? `${dotnetCake}.exe` : dotnetCake;
-
-    if (targetDirectory.containsFile(cakeTool)) {
-      core.info(`The Cake.Tool already exists in ${targetDirectory}, skipping installation`);
-      return;
-    }
-
-    return DotNet.installLocalTool('Cake.Tool', targetDirectory);
+  static async installLocalCakeTool(
+    targetDirectory: ToolsDirectory = new ToolsDirectory(),
+    version?: string
+    ) {
+    return DotNet.installLocalTool('Cake.Tool', dotnetCake, targetDirectory, version);
   }
 
-  static async installLocalTool(toolName: string, targetDirectory: ToolsDirectory = new ToolsDirectory()) {
-    const exitCode = await exec(dotnetToolInstall, ['--tool-path', targetDirectory.path, toolName]);
+  static async installLocalTool(
+    packageId: string,
+    toolName: string,
+    targetDirectory: ToolsDirectory = new ToolsDirectory(),
+    version?: string
+    ) {
+    const toolExecutable = `${toolName}${Platform.isWindows() ? '.exe' : ''}`;
+
+    if (targetDirectory.containsFile(toolExecutable)) {
+      if (version) {
+        const toolInstallPath = path.join(
+              '.store',
+              packageId.toLowerCase(),
+              version.toLowerCase(),
+              'project.assets.json'
+            );
+
+        if (targetDirectory.containsFile(toolInstallPath)) {
+          core.info(`The ${packageId} version ${version} already exists in ${targetDirectory}, skipping installation`);
+          return;
+        }
+
+        const uninstallExitCode = await exec(dotnetToolUnInstall, ['--tool-path', targetDirectory.path, packageId]);
+
+        if (uninstallExitCode != 0) {
+          throw new Error(`Failed to uninstall previous version of ${packageId}. Exit code: ${uninstallExitCode}`);
+        }
+      } else {
+        core.info(`The ${packageId} already exists in ${targetDirectory}, skipping installation`);
+        return;
+      }
+    }
+
+    const versionArg = (version) ? ['--version', version] : [];
+    const installArgs = [...versionArg, '--tool-path', targetDirectory.path, packageId];
+
+    const exitCode = await exec(dotnetToolInstall, installArgs);
 
     if (exitCode != 0) {
-      throw new Error(`Failed to install ${toolName}. Exit code: ${exitCode}`);
+      throw new Error(`Failed to install ${packageId}. Exit code: ${exitCode}`);
     }
   }
 }
