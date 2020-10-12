46bd04bd23651ad8165b452055f979fd3decde20
Jerzy Wickowski <jerzy.wickowski@ajkvision.eu>
2020-10-12 14:40:32 +0200

    add the implemetation
    and install yaargs-parser to parse arguments easier

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,19 @@
-import * as core from '@actions/core';
-import { ToolsDirectory } from './toolsDirectory';
-import * as dotnet from './dotnet';
-import * as cake from './cake';
-import { CakeArgument } from './cakeParameter';
+import * as core from "@actions/core";
+import { ToolsDirectory } from "./toolsDirectory";
+import * as dotnet from "./dotnet";
+import * as cake from "./cake";
+import { CakeArgument } from "./cakeParameter";
+import yargsParser from "yargs-parser";
 
 export async function run() {
   try {
-    const scriptPath = core.getInput('script-path');
-    const version = core.getInput('cake-version');
-    const bootstrap = (core.getInput('cake-bootstrap') || '').toLowerCase() === 'true';
-    const target = new CakeArgument('target', core.getInput('target'));
-    const verbosity = new CakeArgument('verbosity', core.getInput('verbosity'));
+    const scriptPath = core.getInput("script-path");
+    const version = core.getInput("cake-version");
+    const bootstrap =
+      (core.getInput("cake-bootstrap") || "").toLowerCase() === "true";
+    const target = new CakeArgument("target", core.getInput("target"));
+    const verbosity = new CakeArgument("verbosity", core.getInput("verbosity"));
+    const scriptArgs = getScriptArguments();
 
     const toolsDir = new ToolsDirectory();
     toolsDir.create();
@@ -24,10 +27,37 @@ export async function run() {
       await cake.bootstrapScript(scriptPath, toolsDir);
     }
 
-    await cake.runScript(scriptPath, toolsDir, target, verbosity);
+    await cake.runScript(
+      scriptPath,
+      toolsDir,
+      target,
+      verbosity,
+      ...scriptArgs
+    );
   } catch (error) {
     core.setFailed(error.message);
   }
+}
+
+function getScriptArguments(): CakeArgument[] {
+  const result: CakeArgument[] = [];
+  const input: string = core.getInput("script-arguments");
+  if (!input) {
+    return [];
+  }
+
+  const options = yargsParser(input);
+  const optionsKeys: string[] = Object.keys(options);
+  optionsKeys.forEach((optionsKey) => {
+    // we ignore that because of yargs-parser implementation
+    if (optionsKey === "_") {
+      return;
+    }
+
+    const cakeArgument = new CakeArgument(optionsKey, options[optionsKey]);
+    result.push(cakeArgument);
+  });
+  return result;
 }
 
 run();
