254b5d0e583cf59f1ccc7f993a099ddeaded4c3e
Jerzy Wickowski <jerzy.wickowski@ajkvision.eu>
2020-10-14 13:50:51 +0200

    add compiled file

--- dist/index.js
+++ dist/index.js
@@ -1056,16 +1056,23 @@ const core = __importStar(__webpack_require__(470));
 const toolsDirectory_1 = __webpack_require__(348);
 const dotnet = __importStar(__webpack_require__(193));
 const cake = __importStar(__webpack_require__(245));
+// It is disabled, because CakeParameter is an interface and eslit tells us that is 'is defined but never used'. I would suggest option 1 or 2, but can we do it in next PR?
+// There are options:
+// 1. We can change that one eslint rules
+// 2. We can add rules "plugin:@typescript-eslint/recommended" to eslintrc.json, but it will require more changes
+// 3. Leave like it is
+// 4. Use 'function getScriptArguments(): [] {' instead of 'function getScriptArguments(): CakeParameter[] {'
+// eslint-disable-next-line no-unused-vars
 const cakeParameter_1 = __webpack_require__(947);
 const yargs_parser_1 = __importDefault(__webpack_require__(892));
 function run() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
-            const scriptPath = core.getInput("script-path");
-            const version = core.getInput("cake-version");
-            const bootstrap = (core.getInput("cake-bootstrap") || "").toLowerCase() === "true";
-            const target = new cakeParameter_1.CakeArgument("target", core.getInput("target"));
-            const verbosity = new cakeParameter_1.CakeArgument("verbosity", core.getInput("verbosity"));
+            const scriptPath = core.getInput('script-path');
+            const version = core.getInput('cake-version');
+            const bootstrap = (core.getInput('cake-bootstrap') || '').toLowerCase() === 'true';
+            const target = new cakeParameter_1.CakeArgument('target', core.getInput('target'));
+            const verbosity = new cakeParameter_1.CakeArgument('verbosity', core.getInput('verbosity'));
             const scriptArgs = getScriptArguments();
             const toolsDir = new toolsDirectory_1.ToolsDirectory();
             toolsDir.create();
@@ -1084,20 +1091,17 @@ function run() {
 }
 exports.run = run;
 function getScriptArguments() {
-    const result = [];
-    const input = core.getInput("script-arguments");
-    if (!input) {
-        return [];
-    }
+    var input = core.getInput('script-arguments');
     const options = yargs_parser_1.default(input);
     const optionsKeys = Object.keys(options);
-    optionsKeys.forEach((optionsKey) => {
-        // we ignore that because of yargs-parser implementation
-        if (optionsKey === "_") {
-            return;
+    const result = optionsKeys
+        .filter(key => key !== '_') // we ignore that because of yargs-parser implementation
+        .map(optionsKey => {
+        const value = options[optionsKey];
+        if (value === true) {
+            return new cakeParameter_1.CakeSwitch(optionsKey);
         }
-        const cakeArgument = new cakeParameter_1.CakeArgument(optionsKey, options[optionsKey]);
-        result.push(cakeArgument);
+        return new cakeParameter_1.CakeArgument(optionsKey, value);
     });
     return result;
 }
